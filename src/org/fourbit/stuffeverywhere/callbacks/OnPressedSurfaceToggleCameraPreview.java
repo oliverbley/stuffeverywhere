0245b5149ecdd7db1c056de9818670932ba7bd6b
Oliver Bley <commit@ovbly.com>
2012-05-26 12:56:53 +0200

    Fixed copyrights GPL v3.

--- src/org/fourbit/stuffeverywhere/callbacks/OnPressedSurfaceToggleCameraPreview.java
+++ src/org/fourbit/stuffeverywhere/callbacks/OnPressedSurfaceToggleCameraPreview.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ *
+ * Copyright (c) 2012 Oliver Bley. All rights reserved.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the GNU Public License v3
+ * which is available at http://www.gnu.org/licenses/
+ *
+ *******************************************************************************/
 package org.fourbit.stuffeverywhere.callbacks;
 
 import java.io.IOException;
